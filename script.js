caba288c23aa40b1f8ca12040086aa7d5e7747e3
Md Omar Faruk Chowdhury <ofcjim16@gmail.com>
2026-03-02 11:42:41 +0600

    project sec: basic functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// navbar
 const navItems = document.querySelectorAll(".nav-item");
 
 navItems.forEach(item => {
@@ -24,4 +26,32 @@ navLinks.forEach(link => {
     link.addEventListener("click", () => {
         navMenu.classList.remove("show");
     });
+});
+
+
+
+
+
+// hover
+const projectContainers = document.querySelectorAll(".project-container");
+
+projectContainers.forEach(container => {
+  const img = container.querySelector('.project-img');
+  if (!img) return;
+
+  container.addEventListener('mouseenter', () => {
+    img.style.display = 'block';
+  });
+
+  container.addEventListener('mouseleave', () => {
+    img.style.display = 'none';
+  });
+
+  container.addEventListener('mousemove', (e) => {
+    const rect = container.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    img.style.left = x + 'px';
+    img.style.top = y + 'px';
+  });
 });
